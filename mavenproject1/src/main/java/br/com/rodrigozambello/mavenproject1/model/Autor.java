413fedd519886a2e8ae6ad0e93f2c09092724b63
ZambelloRodrigo <101841098+ZambelloRodrigo@users.noreply.github.com>
2023-04-07 22:09:34 -0300

    Criando classe Autor

--- mavenproject1/src/main/java/br/com/rodrigozambello/mavenproject1/model/Autor.java
+++ mavenproject1/src/main/java/br/com/rodrigozambello/mavenproject1/model/Autor.java
@@ -9,5 +9,37 @@ package br.com.rodrigozambello.mavenproject1.model;
  * @author rodrigo
  */
 public class Autor {
+    private int cdAutor;
+    private String nmAutor;
+
+    public Autor() {
+    }
+
+    public Autor(int cdAutor, String nmAutor) {
+        this.cdAutor = cdAutor;
+        this.nmAutor = nmAutor;
+    }
+
+    public int getCdAutor() {
+        return cdAutor;
+    }
+
+    public void setCdAutor(int cdAutor) {
+        this.cdAutor = cdAutor;
+    }
+
+    public String getNmAutor() {
+        return nmAutor;
+    }
+
+    public void setNmAutor(String nmAutor) {
+        this.nmAutor = nmAutor;
+    }
+
+    @Override
+    public String toString() {
+        return "Autor{" + "cdAutor=" + cdAutor + ", nmAutor=" + nmAutor + '}';
+    }
+    
     
 }
