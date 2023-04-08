a20d8a23dfd04fa5d85f9d58e960ad640b4c961b
ZambelloRodrigo <101841098+ZambelloRodrigo@users.noreply.github.com>
2023-04-07 22:04:01 -0300

    Enviando as Classes Criadas

---
+++ mavenproject1/src/main/java/br/com/rodrigozambello/mavenproject1/model/Pais.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.rodrigozambello.mavenproject1.model;
+
+/**
+ *
+ * @author rodrigo
+ */
+public class Pais {
+    
+}
